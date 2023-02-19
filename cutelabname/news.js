a5f823ae10a9e3a95875983243befb33163feb0a
Anoushka Agrawal <90436255+anoushka2@users.noreply.github.com>
2023-02-18 21:18:39 -0800

    Update news.js

--- cutelabname/news.js
+++ cutelabname/news.js
@@ -1,186 +1,192 @@
-/*
-,
-    {
-        'title': '',
-        'date': 'x/x/2022', //MM/DD/YYYY
-        'website_text': '',
-        'website_url': 'https://',
-        'additional_text': ''
-    }
+// /*
+// ,
+//     {
+//         'title': '',
+//         'date': 'x/x/2022', //MM/DD/YYYY
+//         'website_text': '',
+//         'website_url': 'https://',
+//         'additional_text': ''
+//     }
 
 
-*/
+// */
 
-
-var news_data = [
-
-    {
+{
         'title': 'Jon on ChatGPT for The Conversation.',
         'date': '2/2/2023', //MM/DD/YYYY
         'website_text': 'ChatGPT is great – youre just using it wrong',
         'website_url': 'https://theconversation.com/chatgpt-is-great-youre-just-using-it-wrong-198848',
         'additional_text': ''
+}
+// // var news_data = [
 
-    },
-    {
-        'title': 'Computational Journalism work of Alex Spangher covered by ISI news.',
-        'date': '11/29/2022', //MM/DD/YYYY
-        'website_text': 'Rescuing Local Journalism, One AI Tool At A Time',
-        'website_url': 'https://www.isi.edu/news/52459/ai-tool-helps-rescue-local-journalism/',
-        'additional_text': ''
-    },
-    {
-        'title': 'Jon quoted in Wired article.',
-        'date': '11/15/2022', //MM/DD/YYYY
-        'website_text': 'How a Translation App Helped My Mother and Me Say I Love You',
-        'website_url': 'https://www.wired.com/story/how-translation-app-helped-me-say-i-love-you-my-mother/',
-        'additional_text': ''
-    },
+//     {
+//         'title': 'Jon on ChatGPT for The Conversation.',
+//         'date': '2/2/2023', //MM/DD/YYYY
+//         'website_text': 'ChatGPT is great – youre just using it wrong',
+//         'website_url': 'https://theconversation.com/chatgpt-is-great-youre-just-using-it-wrong-198848',
+//         'additional_text': ''
+
+//     },
+//     {
+//         'title': 'Computational Journalism work of Alex Spangher covered by ISI news.',
+//         'date': '11/29/2022', //MM/DD/YYYY
+//         'website_text': 'Rescuing Local Journalism, One AI Tool At A Time',
+//         'website_url': 'https://www.isi.edu/news/52459/ai-tool-helps-rescue-local-journalism/',
+//         'additional_text': ''
+//     },
+//     {
+//         'title': 'Jon quoted in Wired article.',
+//         'date': '11/15/2022', //MM/DD/YYYY
+//         'website_text': 'How a Translation App Helped My Mother and Me Say I Love You',
+//         'website_url': 'https://www.wired.com/story/how-translation-app-helped-me-say-i-love-you-my-mother/',
+//         'additional_text': ''
+//     },
     
-    {
-        'title': 'Jon on moderation for a podcast by the Indian Express newspaper.',
-        'date': '10/20/2022', //MM/DD/YYYY
-        'website_text': 'Mind your language... moderating internet conversations',
-        'website_url': 'https://indianexpress.com/audio/our-own-devices/mind-your-language-moderating-internet-conversations/8221083/',
-        'additional_text': ''
-    },
-    {
-        'title': 'CUTELABNAME has three papers accepted to EMNLP and three papers accepted to Findings of EMNLP.',
-        'date': '10/6/2022', //MM/DD/YYYY
-        'website_text': '',
-        'website_url': '',
-        'additional_text': ''
-    },
-    {
-        'title': 'Katy Felkner in the Daily Trojan for her work in language translation at the border.',
-        'date': '9/20/2022', //MM/DD/YYYY
-        'website_text': 'PhD Student Decodes Language Barriers',
-        'website_url': 'https://dailytrojan.com/2022/09/20/ph-d-student-decodes-language-barriers/',
-        'additional_text': ''
-    },
-    {
-        'title': 'Congratulations to Alexander Spangher for his Honorable Mention award at NAACL 2022.',
-        'date': '7/1/2022', //MM/DD/YYYY
-        'website_text': 'Announcement',
-        'website_url': 'https://2022.naacl.org/blog/best-papers/',
-        'additional_text': ''
-    },
-    {
-        'title': 'CUTELABNAME director Jonathan May has been promoted to Research Associate Professor.',
-        'date': '7/1/2022', //MM/DD/YYYY
-        'website_text': '',
-        'website_url': '',
-        'additional_text': ''
-    },
-    {
-        'title': 'We have four papers at NAACL 2022: one in the main conference, one in Findings, one in the industry track. and one at the Queer in AI Workshop.',
-        'date': '6/15/2022', //MM/DD/YYYY
-        'website_text': 'NAACL 2022 main conference schedule',
-        'website_url': 'https://2022.naacl.org/program/schedule/',
-        'additional_text': ''
-    },
-    {
-        'title': 'Congratulations to Nada Aldarrab on passing her dissertation defense. Nada is the third PhD recipient from CUTELABNAME. She will join King Abdulaziz University as an Assistant Professor.',
-        'date': '6/13/2022', //MM/DD/YYYY
-        'website_text': '',
-        'website_url': '',
-        'additional_text': ''
-        },
-    {
-        'title': 'Congratulations to Katy Felkner for winning an NSF Graduate Research Fellowship.',
-        'date': '4/3/2022', //MM/DD/YYYY
-        'website_text': 'About GRFP',
-        'website_url': 'https://www.nsfgrfp.org/resources/about-grfp/',
-        'additional_text': ''
-    },
-    {
-        'title': 'Congratulations to Thamme Gowda  on passing his dissertation defense. TG is the second PhD recipient from CUTELABNAME. He will join Microsoft as a Research Scientist.',
-        'date': '3/28/2022', //MM/DD/YYYY
-        'website_text': '',
-        'website_url': '',
-        'additional_text': ''
-    },
-    {
-        'title': 'Congratulations to Xusen Yin on passing his dissertation defense. Xusen is the first PhD recipient from CUTELABNAME. He will join Amazon as an Applied Scientist.',
-        'date': '6/14/2021', //MM/DD/YYYY
-        'website_text': '',
-        'website_url': '',
-        'additional_text': ''
-    },
-	  {
-		'title': 'Omegalive.com (Greek)',
-		'date': '8/16/2020', //MM/DD/YYYY
-		'website_text': 'Είσαι σίγουρος ότι μπορείς να καταλάβεις αν μιλάς σε άνθρωπο ή ρομπότ',
-		'website_url': 'https://www.omegalive.com.cy/scitec/2020/08/16/%CE%B5%CE%AF%CF%83%CE%B1%CE%B9-%CF%83%CE%AF%CE%B3%CE%BF%CF%85%CF%81%CE%BF%CF%82-%CF%8C%CF%84%CE%B9-%CE%BC%CF%80%CE%BF%CF%81%CE%B5%CE%AF%CF%82-%CE%BD%CE%B1-%CE%BA%CE%B1%CF%84%CE%B1%CE%BB%CE%AC%CE%B2%CE%B5%CE%B9%CF%82-%CE%B1%CE%BD-%CE%BC%CE%B9%CE%BB%CE%AC%CF%82-%CF%83%CE%B5-%CE%AC%CE%BD%CE%B8%CF%81%CF%89%CF%80%CE%BF-%CE%AE-%CF%81%CE%BF%CE%BC%CF%80%CF%8C%CF%84/',
-		'additional_text': ''
-	},
-	{
-		'title': 'Mindbounce',
-		'date': '8/7/2020', //MM/DD/YYYY
-		'website_text': 'Meet the Chatbot That’s Learning to Improvise',
-		'website_url': 'https://www.mindbounce.com/445861/meet-the-chatbot-thats-learning-to-improvise/',
-		'additional_text': ''
-	},
-	{
-		'title': 'Tech With Gajesh (12-year-old independent tech journalist)',
-		'date': '7/19/2020', //MM/DD/YYYY
-		'website_text': 'Spolin Bot: New Avatar of Chatbot',
-		'website_url': 'https://techwithgajesh.com/spolinbot-the-new-avatar-of-chatbots/',
-		'additional_text': ''
-	},
-	{
-		'title': 'Voicebot.ai',
-		'date': '7/17/2020', //MM/DD/YYYY
-		'website_text': 'New Chatbot Project Turns Conversational AI into an Improv Performance',
-		'website_url': 'https://voicebot.ai/2020/07/17/new-chatbot-project-turns-conversational-ai-into-an-improv-performance/',
-		'additional_text': ''
-	},
-	{
-		'title': 'Techxue.com (Chinese)',
-		'date': '7/16/2020', //MM/DD/YYYY
-		'website_text': '研究人员开发基于即兴的聊天机器人',
-		'website_url': 'http://www.techxue.com/rengongzhineng/202007/18969.html',
-		'additional_text': ''
-	},
-	{
-		'title': 'USC Viterbi',
-		'date': '7/6/2020', //MM/DD/YYYY
-		'website_text': 'Move Over, Siri! USC Viterbi Researchers Develop Improv-based Chatbot',
-		'website_url': 'https://viterbischool.usc.edu/news/2020/07/move-over-siri-usc-viterbi-researchers-develop-improv-based-chatbot/',
-		'additional_text': '. Also <a href="https://chatbotsjournal.com/the-chatbots-journal-newsletter-everything-you-need-to-know-about-top-ai-chatbots-apps-in-2020-b40b2062dae6">chatbotsjournal.com</a>, <a href="https://news8plus.com/transfer-over-siri-researchers-develop-improv-based-chatbot/">news8plus.com</a>, <a href="https://www.newsbreak.com/news/1595625366409/move-over-siri-usc-viterbi-researchers-develop-improv-based-chatbot">newsbreak.com</a>, <a href="https://techxplore.com/news/2020-07-siri-improv-based-chatbot.html">techxplore.com</a>.'
-	},
-	{
-		'title': 'OneCA Civil Affairs Podcast',
-		'date': '4/8/2018', //MM/DD/YYYY
-		'website_text': 'Artificial Intelligence for HA/DR Operations - LORELEI',
-		'website_url': 'https://www.civilaffairsassoc.org/podcast/episode/30fc3431/jon-may-artificial-intelligence-for-hadr-operations-lorelei',
-		'additional_text': ''
-	},
-	{
-		'title': 'Johns Hopkins News-Letter',
-		'date': '12/1/2016', //MM/DD/YYYY
-		'website_text': 'USC prof. analyzes translation software',
-		'website_url': 'https://www.jhunewsletter.com/article/2016/12/usc-prof-analyzes-translation-software',
-		'additional_text': ''
-	},
-	{
-		'title': 'USC Daily Trojan',
-		'date': '1/25/2018', //MM/DD/YYYY
-		'website_text': 'Viterbi automated translation project receives IARPA grant',
-		'website_url': 'https://dailytrojan.com/2018/01/25/viterbi-automated-translation-project-receives-iarpa-grant/',
-		'additional_text': ''
-	},
-	{
-		'title': 'USC Viterbi',
-		'date': '1/8/2018', //MM/DD/YYYY
-		'website_text': 'USC ISI to Develop Translation and Information-Retrieval System for Uncommon Languages',
-		'website_url': 'https://viterbischool.usc.edu/news/2018/01/usc-isi-develop-translation-information-retrieval-system-uncommon-languages/',
-		'additional_text': ''
-	},
-]
+//     {
+//         'title': 'Jon on moderation for a podcast by the Indian Express newspaper.',
+//         'date': '10/20/2022', //MM/DD/YYYY
+//         'website_text': 'Mind your language... moderating internet conversations',
+//         'website_url': 'https://indianexpress.com/audio/our-own-devices/mind-your-language-moderating-internet-conversations/8221083/',
+//         'additional_text': ''
+//     },
+//     {
+//         'title': 'CUTELABNAME has three papers accepted to EMNLP and three papers accepted to Findings of EMNLP.',
+//         'date': '10/6/2022', //MM/DD/YYYY
+//         'website_text': '',
+//         'website_url': '',
+//         'additional_text': ''
+//     },
+//     {
+//         'title': 'Katy Felkner in the Daily Trojan for her work in language translation at the border.',
+//         'date': '9/20/2022', //MM/DD/YYYY
+//         'website_text': 'PhD Student Decodes Language Barriers',
+//         'website_url': 'https://dailytrojan.com/2022/09/20/ph-d-student-decodes-language-barriers/',
+//         'additional_text': ''
+//     },
+//     {
+//         'title': 'Congratulations to Alexander Spangher for his Honorable Mention award at NAACL 2022.',
+//         'date': '7/1/2022', //MM/DD/YYYY
+//         'website_text': 'Announcement',
+//         'website_url': 'https://2022.naacl.org/blog/best-papers/',
+//         'additional_text': ''
+//     },
+//     {
+//         'title': 'CUTELABNAME director Jonathan May has been promoted to Research Associate Professor.',
+//         'date': '7/1/2022', //MM/DD/YYYY
+//         'website_text': '',
+//         'website_url': '',
+//         'additional_text': ''
+//     },
+//     {
+//         'title': 'We have four papers at NAACL 2022: one in the main conference, one in Findings, one in the industry track. and one at the Queer in AI Workshop.',
+//         'date': '6/15/2022', //MM/DD/YYYY
+//         'website_text': 'NAACL 2022 main conference schedule',
+//         'website_url': 'https://2022.naacl.org/program/schedule/',
+//         'additional_text': ''
+//     },
+//     {
+//         'title': 'Congratulations to Nada Aldarrab on passing her dissertation defense. Nada is the third PhD recipient from CUTELABNAME. She will join King Abdulaziz University as an Assistant Professor.',
+//         'date': '6/13/2022', //MM/DD/YYYY
+//         'website_text': '',
+//         'website_url': '',
+//         'additional_text': ''
+//         },
+//     {
+//         'title': 'Congratulations to Katy Felkner for winning an NSF Graduate Research Fellowship.',
+//         'date': '4/3/2022', //MM/DD/YYYY
+//         'website_text': 'About GRFP',
+//         'website_url': 'https://www.nsfgrfp.org/resources/about-grfp/',
+//         'additional_text': ''
+//     },
+//     {
+//         'title': 'Congratulations to Thamme Gowda  on passing his dissertation defense. TG is the second PhD recipient from CUTELABNAME. He will join Microsoft as a Research Scientist.',
+//         'date': '3/28/2022', //MM/DD/YYYY
+//         'website_text': '',
+//         'website_url': '',
+//         'additional_text': ''
+//     },
+//     {
+//         'title': 'Congratulations to Xusen Yin on passing his dissertation defense. Xusen is the first PhD recipient from CUTELABNAME. He will join Amazon as an Applied Scientist.',
+//         'date': '6/14/2021', //MM/DD/YYYY
+//         'website_text': '',
+//         'website_url': '',
+//         'additional_text': ''
+//     },
+// 	  {
+// 		'title': 'Omegalive.com (Greek)',
+// 		'date': '8/16/2020', //MM/DD/YYYY
+// 		'website_text': 'Είσαι σίγουρος ότι μπορείς να καταλάβεις αν μιλάς σε άνθρωπο ή ρομπότ',
+// 		'website_url': 'https://www.omegalive.com.cy/scitec/2020/08/16/%CE%B5%CE%AF%CF%83%CE%B1%CE%B9-%CF%83%CE%AF%CE%B3%CE%BF%CF%85%CF%81%CE%BF%CF%82-%CF%8C%CF%84%CE%B9-%CE%BC%CF%80%CE%BF%CF%81%CE%B5%CE%AF%CF%82-%CE%BD%CE%B1-%CE%BA%CE%B1%CF%84%CE%B1%CE%BB%CE%AC%CE%B2%CE%B5%CE%B9%CF%82-%CE%B1%CE%BD-%CE%BC%CE%B9%CE%BB%CE%AC%CF%82-%CF%83%CE%B5-%CE%AC%CE%BD%CE%B8%CF%81%CF%89%CF%80%CE%BF-%CE%AE-%CF%81%CE%BF%CE%BC%CF%80%CF%8C%CF%84/',
+// 		'additional_text': ''
+// 	},
+// 	{
+// 		'title': 'Mindbounce',
+// 		'date': '8/7/2020', //MM/DD/YYYY
+// 		'website_text': 'Meet the Chatbot That’s Learning to Improvise',
+// 		'website_url': 'https://www.mindbounce.com/445861/meet-the-chatbot-thats-learning-to-improvise/',
+// 		'additional_text': ''
+// 	},
+// 	{
+// 		'title': 'Tech With Gajesh (12-year-old independent tech journalist)',
+// 		'date': '7/19/2020', //MM/DD/YYYY
+// 		'website_text': 'Spolin Bot: New Avatar of Chatbot',
+// 		'website_url': 'https://techwithgajesh.com/spolinbot-the-new-avatar-of-chatbots/',
+// 		'additional_text': ''
+// 	},
+// 	{
+// 		'title': 'Voicebot.ai',
+// 		'date': '7/17/2020', //MM/DD/YYYY
+// 		'website_text': 'New Chatbot Project Turns Conversational AI into an Improv Performance',
+// 		'website_url': 'https://voicebot.ai/2020/07/17/new-chatbot-project-turns-conversational-ai-into-an-improv-performance/',
+// 		'additional_text': ''
+// 	},
+// 	{
+// 		'title': 'Techxue.com (Chinese)',
+// 		'date': '7/16/2020', //MM/DD/YYYY
+// 		'website_text': '研究人员开发基于即兴的聊天机器人',
+// 		'website_url': 'http://www.techxue.com/rengongzhineng/202007/18969.html',
+// 		'additional_text': ''
+// 	},
+// 	{
+// 		'title': 'USC Viterbi',
+// 		'date': '7/6/2020', //MM/DD/YYYY
+// 		'website_text': 'Move Over, Siri! USC Viterbi Researchers Develop Improv-based Chatbot',
+// 		'website_url': 'https://viterbischool.usc.edu/news/2020/07/move-over-siri-usc-viterbi-researchers-develop-improv-based-chatbot/',
+// 		'additional_text': '. Also <a href="https://chatbotsjournal.com/the-chatbots-journal-newsletter-everything-you-need-to-know-about-top-ai-chatbots-apps-in-2020-b40b2062dae6">chatbotsjournal.com</a>, <a href="https://news8plus.com/transfer-over-siri-researchers-develop-improv-based-chatbot/">news8plus.com</a>, <a href="https://www.newsbreak.com/news/1595625366409/move-over-siri-usc-viterbi-researchers-develop-improv-based-chatbot">newsbreak.com</a>, <a href="https://techxplore.com/news/2020-07-siri-improv-based-chatbot.html">techxplore.com</a>.'
+// 	},
+// 	{
+// 		'title': 'OneCA Civil Affairs Podcast',
+// 		'date': '4/8/2018', //MM/DD/YYYY
+// 		'website_text': 'Artificial Intelligence for HA/DR Operations - LORELEI',
+// 		'website_url': 'https://www.civilaffairsassoc.org/podcast/episode/30fc3431/jon-may-artificial-intelligence-for-hadr-operations-lorelei',
+// 		'additional_text': ''
+// 	},
+// 	{
+// 		'title': 'Johns Hopkins News-Letter',
+// 		'date': '12/1/2016', //MM/DD/YYYY
+// 		'website_text': 'USC prof. analyzes translation software',
+// 		'website_url': 'https://www.jhunewsletter.com/article/2016/12/usc-prof-analyzes-translation-software',
+// 		'additional_text': ''
+// 	},
+// 	{
+// 		'title': 'USC Daily Trojan',
+// 		'date': '1/25/2018', //MM/DD/YYYY
+// 		'website_text': 'Viterbi automated translation project receives IARPA grant',
+// 		'website_url': 'https://dailytrojan.com/2018/01/25/viterbi-automated-translation-project-receives-iarpa-grant/',
+// 		'additional_text': ''
+// 	},
+// 	{
+// 		'title': 'USC Viterbi',
+// 		'date': '1/8/2018', //MM/DD/YYYY
+// 		'website_text': 'USC ISI to Develop Translation and Information-Retrieval System for Uncommon Languages',
+// 		'website_url': 'https://viterbischool.usc.edu/news/2018/01/usc-isi-develop-translation-information-retrieval-system-uncommon-languages/',
+// 		'additional_text': ''
+// 	}
+// // ]
 
-// function getNews(){
-// 	return news_data;
-// }
+// // function getNews(){
+// // 	return news_data;
+// // }
 	
 
